00fad3848018700bdda0fc0457937774f202f028
Chien Jui Chen <cxc3787@mavs.uta.edu>
2024-08-24 02:21:50 +0800

    20240823-heroFloating-RWD

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,12 @@ const Hero: React.FC<HeroProps> = ({ onJoinWaitlist }) => {
   return (
     <main className="mt-20 flex-grow flex flex-col justify-center items-center px-4">
       <div className="flex flex-col items-center w-full max-w-2xl">
-        <FlipCircle className="mb-14" onJoinWaitlist={onJoinWaitlist}/>
+      <div className="relative mb-14">
+      <div className="lg:hidden absolute top-[-2.5rem] left-1/2 transform -translate-x-1/2 text-lg text-brand-purple p-2 bg-opacity-50 rounded floating-text" style={{ fontFamily: '"Sebastien Slab Round", serif' }}>
+            Click the plants
+          </div>
+          <FlipCircle onJoinWaitlist={onJoinWaitlist} />
+        </div>
         <h1 className="text-2xl font-bold text-center leading-tight mb-2">
           INFRASTRUCTURE
         </h1>
