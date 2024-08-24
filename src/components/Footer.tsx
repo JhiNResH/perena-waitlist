3409713936fd2fcdb082c1d63720b95c07d18313
Chien Jui Chen <cxc3787@mavs.uta.edu>
2024-08-24 16:47:11 +0800

    20240824-FooterButtonHover

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,9 +20,9 @@ const Footer: React.FC = () => {
             href="https://x.com/Perena__" 
             target="_blank" 
             rel="noopener noreferrer"
-            className="text-brand-purple hover:text-[#d2bb94] transition-colors active:scale-95 active:opacity-80 duration-150"
+            className="text-brand-purple hover:text-[#d2bb94] transition-colors active:scale-95 active:opacity-80 duration-150 group"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor" className="transition-transform duration-300 group-hover:rotate-12">
               <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
             </svg>
           </a>
@@ -30,9 +30,9 @@ const Footer: React.FC = () => {
             href="http://t.me/perena_community"
             target="_blank" 
             rel="noopener noreferrer"
-            className="text-brand-purple hover:text-[#d2bb94] transition-colors active:scale-95 active:opacity-80 duration-150"
+            className="text-brand-purple hover:text-[#d2bb94] transition-colors active:scale-95 active:opacity-80 duration-150 group"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor" className="transition-all duration-300 group-hover:translate-x-1 group-hover:-translate-y-1">
               <path d="M9.78 18.65l.28-4.23 7.68-6.92c.34-.31-.07-.46-.52-.19L7.74 13.3 3.64 12c-.88-.25-.89-.86.2-1.3l15.97-6.16c.73-.33 1.43.18 1.15 1.3l-2.72 12.81c-.19.91-.74 1.13-1.5.71L12.6 16.3l-1.99 1.93c-.23.23-.42.42-.83.42z"/>
             </svg>
           </a>
@@ -40,9 +40,9 @@ const Footer: React.FC = () => {
             href="https://www.instagram.com/perenafi/" 
             target="_blank" 
             rel="noopener noreferrer"
-            className="text-brand-purple hover:text-[#d2bb94] transition-colors active:scale-95 active:opacity-80 duration-150"
+            className="text-brand-purple hover:text-[#d2bb94] transition-colors active:scale-95 active:opacity-80 duration-150 group"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor" className="transition-transform duration-300 group-hover:scale-110">
               <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
             </svg>
           </a>
