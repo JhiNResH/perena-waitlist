8400f4aba3c44270c8acdb2f4ee5bd48f026ebbb
Chien Jui Chen <cxc3787@mavs.uta.edu>
2024-09-09 12:19:24 -0700

    bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './global.css';
 import { Routes, Route } from 'react-router-dom';
 
@@ -7,18 +7,8 @@ import Hero from './components/Hero';
 import Footer from './components/Footer';
 import WaitlistConfirmation from './components/Waitlist/WaitlistConfirmation';
 import Registration from '../src/components/Waitlist/Registration';
-import SplashScreen from './components/SplashScreen';
 
 const App: React.FC = () => {
-  const [showSplash, setShowSplash] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSplash(false);
-    }, 3000); // 3秒後完全移除 SplashScreen
-
-    return () => clearTimeout(timer);
-  }, []);
 
   const MainContent = () => (
     <div className="flex flex-col min-h-screen bg-brand-cream text-brand-purple font-sans text-base leading-base">
@@ -30,7 +20,6 @@ const App: React.FC = () => {
 
   return (
     <>
-      {showSplash && <SplashScreen />}
       <Routes>
         <Route path="/" element={<MainContent />} />
         <Route path="/registration" element={<Registration />} />
